96e69b887b348ec5c3d96d2cffe4d09d9ccced9f
unknown <lhgenryporta27>
2025-03-26 15:32:52 -0500

    version3

--- main.py
+++ main.py
@@ -3,15 +3,11 @@ Determina el mayor de tres numeros ingresando
 por el teclado
 '''
 
-numero1 = int(input("Ingrese el primer número: "))
-numero2 = int(input("Ingrese el segundo número: "))
-numero3 = int(input("Ingrese el tercer número: "))
+numeros = []
+for i in range(1, 4):
+    numero = int(input(f"Ingrese el número {i}: "))
+    numeros.append(numero)
 
-if numero1 > numero2 and numero1 > numero3:
-    mayor = numero1
-elif numero2 > numero1 and numero2 > numero3:
-    mayor = numero2
-else:
-    mayor = numero3
+mayor = sorted(numeros, reverse=True)[0]
 
-print(f"El número mayor entre {numero1}, {numero2} y {numero3} es: {mayor}")
+print(f"El número mayor entre {numeros[0]}, {numeros[1]} y {numeros[2]} es: {mayor}")
