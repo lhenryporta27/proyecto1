8c6bd2c8d7d18fddad58287bcf77d82c8b9df5db
unknown <lhgenryporta27>
2025-03-26 15:27:26 -0500

    version 2

--- main.py
+++ main.py
@@ -3,26 +3,15 @@ Determina el mayor de tres numeros ingresando
 por el teclado
 '''
 
-def intercambiar_valores(numero1, numero2):
-    temporal = numero1
-    numero1 = numero2
-    numero2 = temporal
-    return numero1, numero2
+numero1 = int(input("Ingrese el primer número: "))
+numero2 = int(input("Ingrese el segundo número: "))
+numero3 = int(input("Ingrese el tercer número: "))
 
+if numero1 > numero2 and numero1 > numero3:
+    mayor = numero1
+elif numero2 > numero1 and numero2 > numero3:
+    mayor = numero2
+else:
+    mayor = numero3
 
-numero1=int(input("Ingrese el primero numero: "))
-numero2=int(input("Ingrese el segundo numero: "))
-numero3=int(input("Ingrese el tercer numero: "))
-
-if numero1>numero2:
-    numero1, numero2 = intercambiar_valores(numero1, numero2)
-
-if numero2>numero3:
-    numero2, numero3 = intercambiar_valores(numero1, numero3)
-
-if numero1>numero2:
-    numero2, numero3 = intercambiar_valores(numero1, numero2)
-
-
-print(f"numero ordenados: {numero1}, {numero2}, {numero3}")
-print(f"El mayor es {numero3}")
+print(f"El número mayor entre {numero1}, {numero2} y {numero3} es: {mayor}")
